c7b370111238af7339655aaf03ecc6b97a08db89
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-10-19 12:58:15 +0200

    add logger member to engine

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,9 +1,20 @@
 #include <engine.hpp>
+#include <logger.hpp>
+
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_native_dialog.h>
 
 Engine::Engine() {
-
+    al_init();
+    al_install_keyboard();
+    m_logger.emplace();
 }
 
 Engine::~Engine() {
+    al_uninstall_keyboard();
+    m_logger.reset();
+}
 
+const Logger& Engine::logger() const {
+    return *m_logger;
 }
