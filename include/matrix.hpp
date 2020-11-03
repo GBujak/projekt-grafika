248945e187779250e37a9a9aa1209fdda0aba5bf
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-11-03 13:46:56 +0100

    Fix matrix

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,20 +1,22 @@
 #pragma once
 
+#include <vector>
+
 template <typename T>
 class Matrix {
     std::vector<T> inner;
-    unsigned dimx, dimy;
+    unsigned m_dimx, m_dimy;
 
     public:
     Matrix(unsigned dimx, unsigned dimy)
-        : dimx(dimx), dimy(dimy) {
-        inner.resize(dimx * dimy);
+        : m_dimx(dimx), m_dimy(dimy) {
+        inner.resize(m_dimx * m_dimy);
     }
 
-    unsigned dimx() const { return dimx; }
-    unsigned dimy() const { return dimy; }
+    unsigned dimx() const { return m_dimx; }
+    unsigned dimy() const { return m_dimy; }
 
     T& at(unsigned int x, unsigned int y) {
-        return inner[dimx * y + x];
+        return inner[m_dimx * y + x];
     }
 };
