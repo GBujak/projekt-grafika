f88c60d09e45be39cd7e55c59efe6fc6d1476f00
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-11-03 13:44:43 +0100

    Remove all

--- include/test.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-
-inline void test() {
-    using namespace std;
-    cout << "test()" << endl;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
 #include <iostream>
-#include <test.hpp>
-
-#include <clock.hpp>
-#include <time.h>
 
 int main() {
-    Clock clock = {60};
-    long current_time = time(nullptr);
-    int same_time_count = 0;
-    while (true) {
-        clock.tick();
-        if (time(nullptr) == current_time) same_time_count++;
-        else {
-            std::cout << same_time_count << std::endl;
-            current_time = time(nullptr);
-            same_time_count = 0;
-        }
-    }
-
     return 0;
 }
