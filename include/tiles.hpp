f8595683e7ac77c20a79ac8fd5f31153b783cab8
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-11-05 11:27:45 +0100

    Change Tile to class so it can store where it leads if it's a door

--- include/tiles.hpp
+++ include/tiles.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
-enum class Tile {
-    Space,
-    Wall,
-    Door,
-    Window,
+class Tile {
+    public:
+    enum class Type {
+        Space,
+        Wall,
+        Door,
+        Window,
+    };
+
+    private:
+    unsigned m_door_room_id = 0;
+    Type m_type = Type::Wall;
+
+    public:
+    Tile(Type type, unsigned door_room_id = 0)
+        : m_type(type), m_door_room_id(door_room_id) {}
+
+    Type type() const { return m_type; }
+    unsigned leads_to() const { return m_door_room_id; }
 };
